2f29e8caf7cf1e6409e1101d6ea033175e953796
Hamzafai <155631179+Hamzafai@users.noreply.github.com>
2026-04-17 18:20:14 +0100

    Delete ai_model/training/model.py

--- ai_model/training/model.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# ── Model config ──────────────────────────────────────────────────────────────
-# Using yolov8n.yaml (architecture)
-
-_architecture = 'yolov8n.yaml'
-
-model = YOLO(_architecture)
